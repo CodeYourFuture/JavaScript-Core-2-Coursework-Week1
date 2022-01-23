bbc5fd4e03d0d58892a174021c51f776147810be
timea <timea.reich@gmail.com>
2022-01-23 01:23:47 +0000

    exercises completed

--- 1-exercises/A-accessing-values/exercise3.js
+++ 1-exercises/A-accessing-values/exercise3.js
@@ -20,7 +20,10 @@ let basketballTeam = {
     - console.logs the name of each player on a new line
 */
 
-// write code here
+let listOfPlayers= basketballTeam.topPlayers.sort()
+for (names of listOfPlayers){
+  console.log(names)
+}
 
 
 /* EXPECTED RESULT
