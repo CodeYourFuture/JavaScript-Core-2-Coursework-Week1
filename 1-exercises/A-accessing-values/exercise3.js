e9a7c1becf803db81a4b852c4187c1aa8276e682
Iwona <89700402+IwonaKrysp@users.noreply.github.com>
2022-01-24 15:45:33 +0100

    exercises

--- 1-exercises/A-accessing-values/exercise3.js
+++ 1-exercises/A-accessing-values/exercise3.js
@@ -22,6 +22,13 @@ let basketballTeam = {
 
 // write code here
 
+let players = basketballTeam.topPlayers;
+
+players.sort();
+
+console.log(players[0]);
+console.log(players[1]);
+console.log(players[2]);
 
 /* EXPECTED RESULT
 
