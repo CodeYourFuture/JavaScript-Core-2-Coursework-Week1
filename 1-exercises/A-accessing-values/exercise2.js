7064d73052e2f42f26cb4080dc90e648b8fd2c05
KatarzynaRuksza <91133235+KatarzynaRuksza@users.noreply.github.com>
2022-01-26 18:42:55 +0000

    Update exercise2.js

--- 1-exercises/A-accessing-values/exercise2.js
+++ 1-exercises/A-accessing-values/exercise2.js
@@ -17,7 +17,7 @@ let capitalCities = {
 */
 
 let myCountry = "UnitedKingdom";
-let myCapitalCity; // complete the code
+let myCapitalCity = capitalCities[myCountry];
 
 console.log(myCapitalCity);
 
