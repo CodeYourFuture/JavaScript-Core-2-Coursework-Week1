094fdf28f667165b460a47d2530863aeb848eaf3
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-01-27 19:46:39 +0000

    Add 2-mandatory, recipes.

--- 2-mandatory/1-recipes.js
+++ 2-mandatory/1-recipes.js
@@ -23,3 +23,49 @@
 */
 
 // write code here
+
+class Recipe {
+  constructor(title, servings, ingredients) {
+    this.title = title;
+    this.servings = servings;
+    this.ingredients = ingredients;
+  }
+}
+
+const recipeOne = new Recipe("baklava", 8, ["yufka", "walnuts", "butter"]);
+
+const recipeTwo = new Recipe("tavukgogsu", 10, [
+  "chicken breast",
+  "sugar",
+  "butter",
+  "flour",
+  "milk",
+]);
+
+const recipeThree = new Recipe("karniyarik", 5, [
+  "aubergine",
+  "tomato",
+  "lamb mince",
+  "parsley",
+]);
+
+const recipeFour = new Recipe("kapuska", 4, [
+  "white cabbage",
+  "pork mince",
+  "onions",
+]);
+
+const recipeFive = new Recipe("iskender", 5, [
+  "lamb",
+  "more lamb",
+  "extra lamb",
+  "yogurt",
+]);
+
+const favRecipes = [recipeOne, recipeTwo, recipeThree, recipeFour, recipeFive];
+
+favRecipes.forEach((recipe) => {
+  console.log(`${recipe.title.charAt(0).toUpperCase()}${recipe.title.slice(1)}`);
+  console.log(`Serves: ${recipe.servings}`);
+  console.log(`Ingredients: \n${recipe.ingredients.join("\r\n")}`);
+});
