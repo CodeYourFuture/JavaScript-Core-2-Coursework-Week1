ca0b798c3448bfc90d74e1a72a5676704ffa4d9e
Tersia Koetzee <tersiakoetzee@gmail.com>
2022-08-04 10:39:11 +0200

    exercises done

--- 1-exercises/B-setting-values/exercise2.js
+++ 1-exercises/B-setting-values/exercise2.js
@@ -16,6 +16,8 @@ let student = {
 */
 
 // write code here
+student["attendance"] = 90;
+
 
 /*
   - Write an "if" statement that changes the value of hasPassed to true
@@ -26,8 +28,12 @@ let student = {
 */
 
 // write code here
+if (student["attendance"] >= 90 && student["examScore"] > 60) {
+  student["hasPassed"] = true;
+}
 
 console.log(student);
+
 
 /* EXPECTED RESULT
 
