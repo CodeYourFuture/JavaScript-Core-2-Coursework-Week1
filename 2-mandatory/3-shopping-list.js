0225e123ac3887999fbdd4a5ce0cd1fc2f302163
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2022-08-17 22:50:02 +0200

    Update 3-shopping-list.js

--- 2-mandatory/3-shopping-list.js
+++ 2-mandatory/3-shopping-list.js
@@ -10,7 +10,7 @@
 
   The createShoppingList function should return an object with two properties:
     - "name" of the recipe, which is a string,
-    - "items", which is an arry of the missing ingredients that need to be on the shopping list
+    - "items", which is an array of the missing ingredients that need to be on the shopping list
 */
 
 let pantry = {
