956d0ddef6f935654fc4b7ba29e4bc2a6f870546
BereniceOgundele <102852581+BereniceOgundele@users.noreply.github.com>
2022-09-02 11:05:50 +0200

    2-mandatory
    After struggling with vs code, so happy that I finally had one mandatory test pass, yippee!!!

--- 2-mandatory/1-recipes.js
+++ 2-mandatory/1-recipes.js
@@ -23,3 +23,47 @@
 */
 
 // write code here
+var favRecipe1 = {
+    title: "quick and easy breakfast",
+    serving:2,
+    ingredients: ["four slices wholewheat bread", "butter", "avocado", "salt and pepper to taste"],
+}
+    console.log(favRecipe1.title);
+    console.log(favRecipe1.serving);
+    console.log(favRecipe1.ingredients);
+    
+    var favRecipe2 = {
+    title: "pancakes",
+    serving:2,
+    ingredients: ["one cup of flour", "one egg", "one cup of milk", " a pinch of salt"],
+}
+    console.log(favRecipe2.title);
+    console.log(favRecipe2.serving);
+    console.log(favRecipe2.ingredients);
+
+    var favRecipe3 = {
+    title: "best breakfast",
+    serving:2,
+    ingredients: ["150g bacon", "four slices of wholewheat bread", "200g mushrooms", "two medium sized tomatoes"],
+}
+    console.log(favRecipe3.title);
+    console.log(favRecipe3.serving);
+    console.log(favRecipe3.ingredients);
+
+    var favRecipe4 = {
+    title: "lasagne",
+    serving:6,
+    ingredients: ["1kg steak mince", "one tin of tomato puree", "250g spinach noodles", "250 ml milk", "350g cheese", "2tbs maizena", "one tomato for garnishing" ],
+}
+    console.log(favRecipe4.title);
+    console.log(favRecipe4.serving);
+    console.log(favRecipe4.ingredients);
+
+      var favRecipe5 = {
+    title: "pizza",
+    serving:6,
+    ingredients: ["500g steak mince", "one box of Dr Oetker pizza bases", "500g chicken mince", "tomato", "250g cheddar cheese ", "250g gouda cheese", "yellow pepper", "red pepper", "green pepper", "250g mushrooms", " two slices of feta cheese", "150g olives", "150g pepperdews", "salt and pepper to taste"],
+}
+    console.log(favRecipe5.title);
+    console.log(favRecipe5.serving);
+    console.log(favRecipe5.ingredients);
