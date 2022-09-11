39d66fa2ff68f1e7498003c1928dd264611d363a
JadeJones2022 <100792140+JadeJones2022@users.noreply.github.com>
2022-09-11 10:16:41 +0100

    man 1

--- 2-mandatory/1-recipes.js
+++ 2-mandatory/1-recipes.js
@@ -23,3 +23,78 @@
 */
 
 // write code here
+
+let recipe = {
+  Pancakes:{
+    title: "Pancakes",
+    serves: 4,
+    Ingredients: ["Eggs", "Flour", "Milk", "Butter"] 
+  },
+  LemonCake:{
+    title: "Lemon Cake",
+    serves: 8,
+    Ingredients: ["Eggs", "Flour",  "Butter", "Sugar", "Lemons"] 
+  },
+  Oats:{
+    title: "Overnight Oats",
+    serves: 4,
+    Ingredients: ["Oats", "Apple", "Raisins", "Honey", "Yoghurt", "Milk"] 
+  },
+  Fritatta:{
+    title: "Fritatta",
+    serves: 6,
+    Ingredients: ["Eggs", "Chicken", "Chorizo", "Vegetables"]
+  },
+  Soup:{
+    title: "Soup",
+    serves: 8,
+    Ingredients: ["Onions", "Vegetables", "Chicken", "Stock"]
+
+  }
+
+  
+};
+
+console.log(recipe.Pancakes.title)
+console.log(`Serves: ${recipe.Pancakes.serves}`)
+console.log(`Ingredients:`)
+console.log(recipe.Pancakes.Ingredients[0])
+console.log(recipe.Pancakes.Ingredients[1])
+console.log(recipe.Pancakes.Ingredients[2])
+console.log(recipe.Pancakes.Ingredients[3]);
+
+console.log(recipe.LemonCake.title)
+console.log(`Serves: ${recipe.LemonCake.serves}`)
+console.log(`Ingredients:`)
+console.log(recipe.LemonCake.Ingredients[0])
+console.log(recipe.LemonCake.Ingredients[1])
+console.log(recipe.LemonCake.Ingredients[2])
+console.log(recipe.LemonCake.Ingredients[3])
+console.log(recipe.LemonCake.Ingredients[4]);
+
+
+console.log(recipe.Oats.title)
+console.log(`Serves: ${recipe.Oats.serves}`)
+console.log(`Ingredients:`)
+console.log(recipe.Oats.Ingredients[0])
+console.log(recipe.Oats.Ingredients[1])
+console.log(recipe.Oats.Ingredients[2])
+console.log(recipe.Oats.Ingredients[3])
+console.log(recipe.Oats.Ingredients[4])
+console.log(recipe.Oats.Ingredients[5]);
+
+console.log(recipe.Fritatta.title)
+console.log(`Serves: ${recipe.Fritatta.serves}`)
+console.log(`Ingredients:`)
+console.log(recipe.Fritatta.Ingredients[0])
+console.log(recipe.Fritatta.Ingredients[1])
+console.log(recipe.Fritatta.Ingredients[2])
+console.log(recipe.Fritatta.Ingredients[3]);
+
+console.log(recipe.Soup.title)
+console.log(`Serves: ${recipe.Soup.serves}`)
+console.log(`Ingredients:`)
+console.log(recipe.Soup.Ingredients[0])
+console.log(recipe.Soup.Ingredients[1])
+console.log(recipe.Soup.Ingredients[2])
+console.log(recipe.Soup.Ingredients[3]);
