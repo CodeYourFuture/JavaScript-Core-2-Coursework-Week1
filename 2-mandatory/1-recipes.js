2f0714cc893a5b827cab511ae184c0a62a828942
Nagehan1 <uyumaznagehan@gmail.com>
2022-09-13 00:03:19 +0100

    mandatory 1

--- 2-mandatory/1-recipes.js
+++ 2-mandatory/1-recipes.js
@@ -22,4 +22,65 @@
   You should write and log at least 5 recipes
 */
 
-// write code here
+// write code her
+
+let myFavoriteRecipe1 = {
+    title : "manti",
+    serving : 4,
+    ingredients : [" dough", "minced meat", "tomato sauce","yogurt"]
+  }
+console.log(myFavoriteRecipe1.title);
+console.log(`Serves :${myFavoriteRecipe1.serving}`);
+console.log(`ingredients :`)
+myFavoriteRecipe1.ingredients.forEach( ingredient => console.log(ingredient));
+
+
+let myFavoriteRecipe2 ={
+    title : " Sini Kebab",
+    serving : 4,
+    ingredients : ['minced meat','parsley', 'onion', 'capia pepper', 'spices']
+  }
+
+ console.log(myFavoriteRecipe2.title);
+ console.log(`Serves : ${myFavoriteRecipe2.serving}`) 
+console.log(`ingredients :`)
+myFavoriteRecipe2.ingredients.forEach( ingredient => console.log(ingredient));
+
+
+let myFavoriteRecipe3 ={
+    title : "Pilav",
+    serving : 2,
+    ingredients :['rice', 'butter', 'salt', 'hot water']
+  }
+console.log(myFavoriteRecipe3.title);
+console.log(`Serves : ${myFavoriteRecipe3.serving}`);
+console.log(`ingredients:`)
+myFavoriteRecipe3.ingredients.forEach(ingredient => console.log(ingredient));
+
+
+
+
+let myFavoriteRecipe4 = {
+    title :"icli kofte",
+    serving : 8,
+    ingredients :['fine wheat', 'minced-meat', 'vegetable oil', 'spices']
+   }
+
+ console.log(myFavoriteRecipe4.title);
+ console.log(`Serves : ${myFavoriteRecipe4.serving}`);
+ console.log(`ingredients`)
+ myFavoriteRecipe4.ingredients.forEach(ingredient => console.log(ingredient))  ;
+
+ 
+ let myFavoriteRecipe5={
+      title :"ezogelin soup",
+      serving : 5,
+      ingredients : ['lentil','rice',' wheat', 'onion', 'salt', 'spices', 'hot water']
+    }
+
+console.log(myFavoriteRecipe5.title);
+ console.log(`Serves : ${myFavoriteRecipe5.serving}`);
+ console.log(`ingredients`)
+ myFavoriteRecipe5.ingredients.forEach(ingredient => console.log(ingredient))  ;
+
+ 
