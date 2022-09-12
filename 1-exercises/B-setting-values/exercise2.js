6830a392328636b501317e02472d1bc4825ef230
Arbeits-Sachen <105115862+Arbeits-Sachen@users.noreply.github.com>
2022-09-12 08:09:33 +0100

    Update exercise2.js

--- 1-exercises/B-setting-values/exercise2.js
+++ 1-exercises/B-setting-values/exercise2.js
@@ -3,39 +3,43 @@
   You can also change the value assigned to a property using bracket notation.
 */
 
-let student = {
-  name: "Reshma Saujani",
-  examScore: 65,
-  hasPassed: false
+let student =
+{
+    name: "Reshma Saujani",
+    examScore: 65,
+    hasPassed: false
 };
 
 /*
-  Using bracket notation
+Using bracket notation
     - Add a property to the student object for attendance
     - Set the value of attendance to 90
 */
 
 // write code here
+student[attendance = 90];
 
 /*
-  - Write an "if" statement that changes the value of hasPassed to true
-      if the student has attendance that is equal or greater than 90
-      AND
-      exam score is above 60.
-  - Use bracket notation to change the value of hasPassed
+- Write an "if" statement that changes the value of hasPassed to true
+    if the student has attendance that is equal or greater than 90
+    AND
+    exam score is above 60.
+- Use bracket notation to change the value of hasPassed
 */
 
 // write code here
+if(attendance >= 90 && examScore > 60)
+{
+    student.hasPassed = true;
+}
 
 console.log(student);
 
 /* EXPECTED RESULT
-
-  {
+{
     name: "Reshma Saujani",
     examScore: 65,
     hasPassed: true,
     attendance: 90
-  }
-
+}
 */
