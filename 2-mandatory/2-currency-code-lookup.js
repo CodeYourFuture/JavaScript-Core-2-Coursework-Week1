1f5d8d63eb529e4c4f5f7f91b2143b8f7be68894
Arbeits-Sachen <105115862+Arbeits-Sachen@users.noreply.github.com>
2022-09-12 09:11:34 +0100

    Update 2-currency-code-lookup.js

--- 2-mandatory/2-currency-code-lookup.js
+++ 2-mandatory/2-currency-code-lookup.js
@@ -1,24 +1,26 @@
 /*
   Objects can be thought of as key/value storage, like a dictionary or a 'lookup'.
-
   You have a variable called COUNTRY_CURRENCY_CODES which is an array of arrays.
   Each inner array contains a country code and its currency code.
-
   Write a function called createLookup that
     - takes COUNTRY_CURRENCY_CODES as an argument (remember, it's an array of arrays)
     - returns a new object where the keys are the country codes and the values are the currency codes
     Hint: you'll need to use bracket notation to add new key/value pairs to the object
 */
 
-const COUNTRY_CURRENCY_CODES = [
-  ["GB", "GBP"],
-  ["DE", "EUR"],
-  ["NG", "NGN"],
-  ["MX", "MXN"],
+const COUNTRY_CURRENCY_CODES =
+[
+    ["GB", "GBP"],
+    ["DE", "EUR"],
+    ["NG", "NGN"],
+    ["MX", "MXN"],
 ];
 
-function createLookup(countryCurrencyCodes) {
-  // write code here
+function createLookup(countryCurrencyCodes)
+{
+    let allCurrencies = {};
+    countryCurrencyCodes.forEach(ccc => {allCurrencies[ccc[0]] = ccc[1];});
+    return allCurrencies;
 }
 
 /* ======= TESTS - DO NOT MODIFY =====
