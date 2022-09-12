81bb5980f1f1d1a534392a8dc3404ea27dd779ee
Arbeits-Sachen <105115862+Arbeits-Sachen@users.noreply.github.com>
2022-09-12 07:54:19 +0100

    Update exercise3.js

--- 1-exercises/A-accessing-values/exercise3.js
+++ 1-exercises/A-accessing-values/exercise3.js
@@ -2,31 +2,35 @@
   An object's properties can have values that are other objects or arrays.
 */
 
-let basketballTeam = {
-  name: "Chicago Bulls",
-  numberOfPlayers: 15,
-  topPlayers: ["Michael Jordan", "Scottie Pippen", "Dennis Rodman"],
-  homeStadium: {
-    name: "United Center",
-    capacity: 21000,
-    address: "1901 W Madison St",
-  },
+let basketballTeam =
+{
+    name: "Chicago Bulls",
+    numberOfPlayers: 15,
+    topPlayers: ["Michael Jordan", "Scottie Pippen", "Dennis Rodman"],
+    homeStadium:
+    {
+      name: "United Center",
+      capacity: 21000,
+      address: "1901 W Madison St",
+    }
 };
 
 /*
-  Write code that
+Write code that
     - accesses the basketball team's top players array
     - sorts the top players in alphabetical order
     - console.logs the name of each player on a new line
 */
 
 // write code here
-
+basketballTeam.topPlayers.sort();
+for (let i = 0; i < basketballTeam.topPlayers.length; i++)
+{
+    console.log(basketballTeam.topPlayers[i]);
+}
 
 /* EXPECTED RESULT
-
-  Dennis Rodman
-  Michael Jordan
-  Scottie Pippen
-
+Dennis Rodman
+Michael Jordan
+Scottie Pippen
 */
