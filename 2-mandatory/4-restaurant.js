9553a097eeed6c3f799f053a96533d52e908aed5
Alex Phillip <alexjoelphillip@gmail.com>
2022-09-13 11:54:31 +0100

    completed mandatory 4

--- 2-mandatory/4-restaurant.js
+++ 2-mandatory/4-restaurant.js
@@ -21,6 +21,8 @@ const MENU = {
 
 let cashRegister = {
   // write code here
+  orderBurger: (balance) => (balance - MENU.burger >= 0) ? balance -= MENU.burger : balance,
+  orderFalafel: (balance) => (balance - MENU.falafel >= 0) ? balance -= MENU.falafel : balance
 }
 
 /* ======= TESTS - DO NOT MODIFY =====
