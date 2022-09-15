29e6714f1bb4566837ead2d1531c3fc92a989074
Fatemeh Rahimi <fa.rahimi5475@gmail.com>
2022-09-15 17:48:49 +0100

    compeleted

--- 2-mandatory/4-restaurant.js
+++ 2-mandatory/4-restaurant.js
@@ -1,55 +1,48 @@
 /*
   You are at a restaurant that has two items on the menu, burger and falafel.
   The menu object includes these two items and their price.
-
   You have an object cashRegister which handles charging for ordered items but it is missing some methods to do this.
-
   Add a method to the cashRegister object
     - the method name should be orderBurger
     - the method should take one argument called balance, which is a number
     - the method should check if there is enough money in the balance to order a burger (i.e. will the balance be greater than or equal to 0 if the burger is ordered)
     - if there is enough money in the balance to order the burger, then the price of the burger should be subtracted from the balance
     - the method should return the new balance
-
   Add another method to the cashRegister object which is called orderFalafel and handles ordering a falafel, in the same way as ordering a burger.
 */
 
-const pantry = {
-  fridgeContents: ["butter", "milk"],
-  cupboardContents: ["salt", "tinned tomatoes", "oregano"],
+const MENU = {
+  burger: 6.5,
+  falafel: 7.25,
 };
 
-const createShoppingList = (recipe) => ({
-  name: recipe.name,
-  items: recipe.ingredients.filter((e) => !pantry.fridgeContents.includes(e) && !pantry.cupboardContents.includes(e)),
-});
+const cashRegister = {
+  orderBurger: (balance) => (balance >= MENU.burger ? balance - MENU.burger : balance),
+  orderFalafel: (balance) => (balance >= MENU.falafel ? balance - MENU.falafel : balance),
+};
 
 /* ======= TESTS - DO NOT MODIFY =====
-- To run the tests for this exercise, run `npm test -- --testPathPattern 3-shopping-list.js`
+- To run the tests for this exercise, run `npm test -- --testPathPattern 4-restaurant.js`
 - To run all exercises/tests in the mandatory folder, run `npm test`
 - (Reminder: You must have run `npm install` one time before this will work!)
 */
 
-test("createShoppingList works for pancakes recipe", () => {
-  const recipe1 = {
-    name: "pancakes",
-    ingredients: ["flour", "salt", "milk", "eggs", "vegetable oil"],
-  };
-
-  expect(createShoppingList(recipe1)).toEqual({
-    name: "pancakes",
-    items: ["flour", "eggs", "vegetable oil"],
-  });
+test("orderBurger subtracts 6.5 from balance", () => {
+  const balance = 6.5;
+  expect(cashRegister.orderBurger(balance)).toEqual(0);
 });
 
-test("createShoppingList works for margherita pizza recipe", () => {
-  const recipe2 = {
-    name: "margherita pizza",
-    ingredients: ["flour", "salt", "yeast", "tinned tomatoes", "oregano", "mozarella"],
-  };
+test("orderFalafel subtracts 7.25 from balance", () => {
+  const balance = 7.25;
+  expect(cashRegister.orderFalafel(balance)).toEqual(0);
+});
 
-  expect(createShoppingList(recipe2)).toEqual({
-    name: "margherita pizza",
-    items: ["flour", "yeast", "mozarella"],
-  });
+test("orderBurger will not subtract from balance if balance is too low", () => {
+  const balance = 6.49;
+  expect(cashRegister.orderBurger(balance)).toEqual(6.49);
+});
+
+test("orderFalafel will not subtract from balance if balance is too low", () => {
+  const balance = 7.24;
+  expect(cashRegister.orderFalafel(balance)).toEqual(7.24);
 });
