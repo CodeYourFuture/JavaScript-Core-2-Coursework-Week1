0ad3ed00005549502ade338dde1d189a86cfc4ef
Abdirahim Hussein <drahimhussein@gmail.com>
2022-09-15 17:43:39 +0100

    finished last task all npm test are passed

--- 2-mandatory/4-restaurant.js
+++ 2-mandatory/4-restaurant.js
@@ -21,8 +21,19 @@ const MENU = {
 
 let cashRegister = {
   // write code here
-}
-
+    orderBurger: function (balance) {
+      if (balance >= MENU.burger) {
+        balance -= MENU.burger;
+      }
+      return balance;
+    },
+    orderFalafel: function(balance) {
+      if (balance >= MENU.falafel) {
+        balance -= MENU.falafel
+      }
+      return balance;
+    }
+  };
 /* ======= TESTS - DO NOT MODIFY =====
 - To run the tests for this exercise, run `npm test -- --testPathPattern 4-restaurant.js`
 - To run all exercises/tests in the mandatory folder, run `npm test`
