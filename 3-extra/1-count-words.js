707af03ed123c15bbb058adada88fb3a019a13b4
Dawit Abraha <akdawit1@gmail.com>
2022-09-16 23:40:47 +0100

    Completed Extra task

--- 3-extra/1-count-words.js
+++ 3-extra/1-count-words.js
@@ -26,7 +26,15 @@
 function countWords(string) {
   const wordCount = {};
 
-  // write code here
+  if (string === "") {
+    return wordCount;
+  } else {
+    string.split(" ").forEach((word) => {
+      if (wordCount[word]) {
+        wordCount[word] = wordCount[word] + 1;
+      } else wordCount[word] = 1;
+    });
+  }
 
   return wordCount;
 }
@@ -46,9 +54,13 @@ test("Code works for a small string", () => {
 });
 
 test("A string with, some punctuation", () => {
-  expect(countWords("A string with, some punctuation")).toEqual(
-    { A: 1, string: 1, "with,": 1, some: 1, punctuation: 1 }
-  );
+  expect(countWords("A string with, some punctuation")).toEqual({
+    A: 1,
+    string: 1,
+    "with,": 1,
+    some: 1,
+    punctuation: 1,
+  });
 });
 
 test("Empty string", () => {
@@ -56,7 +68,11 @@ test("Empty string", () => {
 });
 
 test("Example task string", () => {
-  expect(countWords("you're braver than you believe, stronger than you seem, and smarter than you think")).toEqual({
+  expect(
+    countWords(
+      "you're braver than you believe, stronger than you seem, and smarter than you think"
+    )
+  ).toEqual({
     "you're": 1,
     and: 1,
     "believe,": 1,
