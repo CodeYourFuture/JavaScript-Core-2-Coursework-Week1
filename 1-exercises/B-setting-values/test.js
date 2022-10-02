269675ddd4ef94e30aec24c903c5fd76437112f8
Shimaadnan <78430611+Shimaadnan@users.noreply.github.com>
2022-10-02 21:42:13 +0100

    B-setting-value

---
+++ 1-exercises/B-setting-values/test.js
@@ -0,0 +1,18 @@
+let capitalCities = {
+  UnitedKingdom: {
+    name: "London",
+    population: 20,
+  },
+  China: {
+    name: "Beijing",
+  },
+};
+
+let myLaptop = {
+  model: "HP",
+  color: "grey",
+};
+myLaptop.screenSize = 16;
+// console.log(myLaptop);
+// capitalCities.UnitedKingdom.population = 99999;
+console.log(capitalCities);
