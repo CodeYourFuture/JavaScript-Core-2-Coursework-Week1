98bb7971902bb6c0273142c3682ab3961bb39bca
Rebwar <anvar.azizi21@gmail.com>
2022-10-04 20:09:04 +0100

    3-extra/1-count-words

--- 3-extra/1-count-words.js
+++ 3-extra/1-count-words.js
@@ -24,9 +24,17 @@
 */
 
 function countWords(string) {
-  const wordCount = {};
-
-  // write code here
+  let words = string.split(" ");
+  let wordCount = {};
+  if (string.length === 0) {
+    return (wordCount = {});
+  } else {
+    for (let i = 0; i < words.length; i++) {
+      let currentWordCount = wordCount[words[i]];
+      let count = currentWordCount ? currentWordCount : 0;
+      wordCount[words[i]] = count + 1;
+    }
+  }
 
   return wordCount;
 }
@@ -46,9 +54,13 @@ test("Code works for a small string", () => {
 });
 
 test("A string with, some punctuation", () => {
-  expect(countWords("A string with, some punctuation")).toEqual(
-    { A: 1, string: 1, "with,": 1, some: 1, punctuation: 1 }
-  );
+  expect(countWords("A string with, some punctuation")).toEqual({
+    A: 1,
+    string: 1,
+    "with,": 1,
+    some: 1,
+    punctuation: 1,
+  });
 });
 
 test("Empty string", () => {
@@ -56,7 +68,11 @@ test("Empty string", () => {
 });
 
 test("Example task string", () => {
-  expect(countWords("you're braver than you believe, stronger than you seem, and smarter than you think")).toEqual({
+  expect(
+    countWords(
+      "you're braver than you believe, stronger than you seem, and smarter than you think"
+    )
+  ).toEqual({
     "you're": 1,
     and: 1,
     "believe,": 1,
