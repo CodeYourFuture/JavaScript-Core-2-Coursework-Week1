1e0ffcdb5611293fe5199b893697c1a1df8fa70f
Shimaadnan <78430611+Shimaadnan@users.noreply.github.com>
2022-10-05 11:58:53 +0100

    Mand Three

--- 2-mandatory/3-shopping-list.js
+++ 2-mandatory/3-shopping-list.js
@@ -19,8 +19,22 @@ let pantry = {
 };
 
 function createShoppingList(recipe) {
-  // write code here
+  let final = new Object();
+  let result = [];
+  for (ingredient of recipe.ingredients) {
+    if (
+      !pantry.fridgeContents.includes(ingredient) &&
+      !pantry.cupboardContents.includes(ingredient)
+    ) {
+      result.push(ingredient);
+    }
+  }
+  final["name"] = recipe.name;
+  final["items"] = result;
+  return final;
 }
+
+// write code here
 
 /* ======= TESTS - DO NOT MODIFY =====
 - To run the tests for this exercise, run `npm test -- --testPathPattern 3-shopping-list.js`
@@ -43,11 +57,18 @@ test("createShoppingList works for pancakes recipe", () => {
 test("createShoppingList works for margherita pizza recipe", () => {
   let recipe2 = {
     name: "margherita pizza",
-    ingredients: ["flour", "salt", "yeast", "tinned tomatoes", "oregano", "mozarella"],
+    ingredients: [
+      "flour",
+      "salt",
+      "yeast",
+      "tinned tomatoes",
+      "oregano",
+      "mozarella",
+    ],
   };
 
   expect(createShoppingList(recipe2)).toEqual({
     name: "margherita pizza",
-    items: ["flour", "yeast", "mozarella"]
+    items: ["flour", "yeast", "mozarella"],
   });
 });
