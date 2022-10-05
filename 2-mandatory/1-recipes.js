33faea49e270001278f24fe8560f2e83f358d6e0
khmdagal <81495872+khmdagal@users.noreply.github.com>
2022-10-05 22:21:35 +0100

    Mandatory 1

--- 2-mandatory/1-recipes.js
+++ 2-mandatory/1-recipes.js
@@ -23,3 +23,29 @@
 */
 
 // write code here
+
+const myCoffeeLatte = {
+  coffeeTpe: 'Ethiopian',
+  isOrganic: true,
+  isRoast: true,
+  roastLevel: "medium",
+  shots: 1.5,
+  ingredients: ['Milk', 'Caremel', 'No Sugar'],
+  MakeCoffee: function () {
+    let haveCoffe;
+    if (this.coffeeTpe === 'Ethiopian' && this.isOrganic === true && this.isRoast === true && this.roastLevel === 'medium') {
+      console.log('Injoy your ☕ Caramel Latte 😀')
+    } else {
+      console.log("Not my COFFEE😉");
+    }
+  }
+}
+console.log(myCoffeeLatte) // this is loging the whole obeject and it's properties.
+
+myCoffeeLatte.ingredients.filter(elements => console.log(elements)) // this is to show the ingredients one in each line.
+
+myCoffeeLatte.MakeCoffee(); // calling makeCoffee method and see how it works.
+
+myCoffeeLatte.isOrganic = false; //this is to check if the conditon is working 
+
+myCoffeeLatte.MakeCoffee(); // result after one of the conditions is assign to a false value.
