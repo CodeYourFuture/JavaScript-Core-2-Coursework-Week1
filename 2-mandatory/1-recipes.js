d04d77a17e9584851ffec14770cf29c1fffc33ef
ShimenAfshar <afsharshimen@gmail.com>
2022-10-06 20:02:02 +0100

    mandatory 1.2

--- 2-mandatory/1-recipes.js
+++ 2-mandatory/1-recipes.js
@@ -23,3 +23,32 @@
 */
 
 // write code here
+
+const logRecipe = (recipe) => {
+  console.log(recipe.title);
+  console.log(`Serves: ${recipe.servings}`);
+  console.log("Ingredients:");
+  recipe.ingredients.forEach((ingredient) => console.log(ingredient));
+}
+
+let recipe1 = {
+  title: "pizza",
+  servings: 1,
+  ingredients: ["bread", "ham", "cheese"]
+};
+
+let recipe2 = {
+  title: "sandwich",
+  servings: 1,
+  ingredients: ["baguette", "sausage", "ketchup", "mustard"],
+};
+
+let recipe3 = {
+  title: "chicken",
+  servings: 2,
+  ingredients: ["cabbage", "tomato", "cucumber", "mayo"],
+};
+
+logRecipe(recipe1);
+logRecipe(recipe2);
+logRecipe(recipe3);
