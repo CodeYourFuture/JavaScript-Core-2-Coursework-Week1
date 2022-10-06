46f1636278b348db663cf8f36c114132f335dd89
fatih-celebi <celebifatih.c1@gmail.com>
2022-10-06 01:12:40 +0100

    Complete Mandatory-1

--- 2-mandatory/1-recipes.js
+++ 2-mandatory/1-recipes.js
@@ -23,3 +23,55 @@
 */
 
 // write code here
+
+let myFavoriteRecipe1 = {
+  title: "Felafel",
+  serving: 2, 
+  ingredients: [
+    "Chicken", "Salad", "Patatos",
+  ]
+}
+let myFavoriteRecipe2 = {
+  title: "Soup",
+  serving: 2, 
+  ingredients: [
+    "Chicken", "Water", "Lentil",
+  ]
+}
+let myFavoriteRecipe3 = {
+  title: "Pizza",
+  serving: 2, 
+  ingredients: [
+    "Dough", "Cheese", "Toppings",
+  ]
+}
+let myFavoriteRecipe4 = {
+  title: "Kebap",
+  serving: 2, 
+  ingredients: [
+    "Lamp", "Patatos", "Tomatos",
+  ]
+}
+let myFavoriteRecipe5 = {
+  title: "Rice",
+  serving: 2, 
+  ingredients: [
+    "Rice", "Salt", "Oil",
+  ]
+}
+function printRecipe(recipe) {
+console.log(myFavoriteRecipe1.title);
+console.log(`Serving: ${myFavoriteRecipe1.serving}`);
+console.log("Ingredients:");
+for (let index = 0; index < recipe.length; index++) {
+  const element = recipe[index];
+  console.log(element);
+}
+}
+
+printRecipe(myFavoriteRecipe1.ingredients);
+printRecipe(myFavoriteRecipe2.ingredients);
+printRecipe(myFavoriteRecipe3.ingredients);
+printRecipe(myFavoriteRecipe4.ingredients);
+printRecipe(myFavoriteRecipe5.ingredients);
+
