911bb774e63c347e1a85e42fb4b5a756b1cd5d9e
Refik Gulak <74262442+Refik48@users.noreply.github.com>
2022-10-12 01:48:32 +0100

    Mandatory done

--- 2-mandatory/3-shopping-list.js
+++ 2-mandatory/3-shopping-list.js
@@ -19,7 +19,12 @@ let pantry = {
 };
 
 function createShoppingList(recipe) {
-  // write code here
+let obj = {};
+obj.name = recipe.name;
+obj.items = recipe.ingredients.filter( (e) =>{
+  return (!(pantry.fridgeContents.includes(e)) &&!(pantry.cupboardContents.includes(e)))
+})
+return obj;
 }
 
 /* ======= TESTS - DO NOT MODIFY =====
@@ -43,11 +48,18 @@ test("createShoppingList works for pancakes recipe", () => {
 test("createShoppingList works for margherita pizza recipe", () => {
   let recipe2 = {
     name: "margherita pizza",
-    ingredients: ["flour", "salt", "yeast", "tinned tomatoes", "oregano", "mozarella"],
+    ingredients: [
+      "flour",
+      "salt",
+      "yeast",
+      "tinned tomatoes",
+      "oregano",
+      "mozarella",
+    ],
   };
 
   expect(createShoppingList(recipe2)).toEqual({
     name: "margherita pizza",
-    items: ["flour", "yeast", "mozarella"]
+    items: ["flour", "yeast", "mozarella"],
   });
 });
