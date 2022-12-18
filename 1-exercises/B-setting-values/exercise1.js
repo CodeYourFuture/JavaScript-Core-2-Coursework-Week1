2228e33886a5f8bc771c79d8db56c7de05a93d00
OlushoreOdude <108965036+OlushoreOdude@users.noreply.github.com>
2022-12-18 17:40:28 +0000

    Update exercise1.js

--- 1-exercises/B-setting-values/exercise1.js
+++ 1-exercises/B-setting-values/exercise1.js
@@ -22,13 +22,11 @@ let capitalCities = {
     - Add a population of 9750000 to Peru's capital city.
 */
 
-// write code here
-// Using dot notation:
-//     - Change the value of UnitedKingdom's capital city population to 8980000.
-//     - Add the property for population to China's capital city and set the value to 21500000.
-//     - Add the country "Peru" to capitalCities object.
-//     - Add a name of "Lima" to Peru's capital city.
-//     - Add a population of 9750000 to Peru's capital city.
+capitalCities.UnitedKingdom.population = 8980000;
+capitalCities.China.population = 21500000;
+capitalCities.Peru = {};
+capitalCities.Peru.name = "lima";
+capitalCities.Peru.population = 9750000;
 console.log(capitalCities);
 
 /* EXPECTED RESULT
