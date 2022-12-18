7720eb0d6c06f025167e93e53fa5dc5d9eebee27
Mari Azh <Marzi.aghchay@gmail.com>
2022-12-18 22:10:57 +0000

    Update exercise2.js

--- 1-exercises/B-setting-values/exercise2.js
+++ 1-exercises/B-setting-values/exercise2.js
@@ -16,6 +16,7 @@ let student = {
 */
 
 // write code here
+student["attendance"] = 90;
 
 /*
   - Write an "if" statement that changes the value of hasPassed to true
@@ -26,7 +27,9 @@ let student = {
 */
 
 // write code here
-
+student["attendance"] >= 90 && student["examScore"] > 60
+? (student["hasPassed"] = true)
+: (student["hasPassed"] = false);
 console.log(student);
 
 /* EXPECTED RESULT
