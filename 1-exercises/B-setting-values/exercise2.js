36291ad224d883db1653319f9852da07523354aa
Zahraa <113244988+ZahraaTayyar@users.noreply.github.com>
2022-12-19 16:08:59 +0000

    exercises completed

--- 1-exercises/B-setting-values/exercise2.js
+++ 1-exercises/B-setting-values/exercise2.js
@@ -17,6 +17,8 @@ let student = {
 
 // write code here
 
+student.attendance = 90;
+
 /*
   - Write an "if" statement that changes the value of hasPassed to true
       if the student has attendance that is equal or greater than 90
@@ -26,6 +28,10 @@ let student = {
 */
 
 // write code here
+
+if(student.attendance >= 90 && student.examScore > 60) {
+  student.hasPassed = true; 
+}
 
 console.log(student);
 
