b47f5fae0c28273bba47762ad24a2e14db795b48
MacOS <macos@blessing.home>
2022-12-20 14:44:23 +0000

    feat: adding mandatory solutons

--- 2-mandatory/4-restaurant.js
+++ 2-mandatory/4-restaurant.js
@@ -21,6 +21,18 @@ const MENU = {
 
 let cashRegister = {
   // write code here
+  orderBurger : function (balance){
+                  if (balance>=MENU.burger){
+                    return balance = balance - MENU.burger
+                  }
+                  return balance
+                },
+  orderFalafel : function (balance){
+                  if (balance>=MENU.falafel){
+                    return balance - MENU.falafel
+                  }
+                  return balance
+                }
 }
 
 /* ======= TESTS - DO NOT MODIFY =====
