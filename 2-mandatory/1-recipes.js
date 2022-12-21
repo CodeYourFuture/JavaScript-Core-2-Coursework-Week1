e74ef13af4d64d4c1eadcdbc176e49f3cf69f431
Mari Azh <Marzi.aghchay@gmail.com>
2022-12-21 10:35:11 +0000

    Update 1-recipes.js

--- 2-mandatory/1-recipes.js
+++ 2-mandatory/1-recipes.js
@@ -30,11 +30,11 @@ const cutletFood1={
   Serves: 2,
   ingredients:["carrots1","potatoes1", "broccoli1"]
 }
-console.log(cutletFood1.title);
-console.log(`serves: ${cutletFood1["Serves"]}`);
-cutletFood1.ingredients.forEach((ingredient) => {
-  console.log(ingredient);
-});
+// console.log(cutletFood1.title);
+// console.log(`serves: ${cutletFood1["Serves"]}`);
+// cutletFood1.ingredients.forEach((ingredient) => {
+//   console.log(ingredient);
+// });
 
 //recipe 2
 const cutletFood2={
@@ -42,22 +42,22 @@ const cutletFood2={
   Serves: 4,
   ingredients:["carrots2","potatoes2", "broccoli2"]
 }
-console.log(cutletFood2.title);
-console.log(`serves: ${cutletFood2["Serves"]}`);
-cutletFood2.ingredients.forEach((ingredient) => {
-  console.log(ingredient);
-});
+// console.log(cutletFood2.title);
+// console.log(`serves: ${cutletFood2["Serves"]}`);
+// cutletFood2.ingredients.forEach((ingredient) => {
+//   console.log(ingredient);
+// });
 // recipe 3
 const cutletFood3={
   title:"cutlet3",
   Serves: 21,
   ingredients:["carrots3","potatoes3", "broccoli3"]
 }
-console.log(cutletFood3.title);
-console.log(`serves: ${cutletFood3["Serves"]}`);
-cutletFood3.ingredients.forEach((ingredient) => {
-  console.log(ingredient);
-});
+// console.log(cutletFood3.title);
+// console.log(`serves: ${cutletFood3["Serves"]}`);
+// cutletFood3.ingredients.forEach((ingredient) => {
+//   console.log(ingredient);
+// });
 
 //recipe 4
 const cutletFood4={
@@ -65,11 +65,11 @@ const cutletFood4={
   Serves: 8,
   ingredients:["carrots4","potatoes4", "broccoli4"]
 }
-console.log(cutletFood4.title);
-console.log(`serves: ${cutletFood4["Serves"]}`);
-cutletFood4.ingredients.forEach((ingredient) => {
-  console.log(ingredient);
-});
+// console.log(cutletFood4.title);
+// console.log(`serves: ${cutletFood4["Serves"]}`);
+// cutletFood4.ingredients.forEach((ingredient) => {
+//   console.log(ingredient);
+// });
 
 //recipe 5
 const cutletFood5={
@@ -77,10 +77,13 @@ const cutletFood5={
   Serves: 9,
   ingredients:["carrots5","potatoes5", "broccoli5"]
 }
-console.log(cutletFood5.title);
-console.log(`serves: ${cutletFood5["Serves"]}`);
-cutletFood5.ingredients.forEach((ingredient) => {
-  console.log(ingredient);
-});
+// console.log(cutletFood5.title);
+// console.log(`serves: ${cutletFood5["Serves"]}`);
+// cutletFood5.ingredients.forEach((ingredient) => {
+//   console.log(ingredient);
+// });
 
-
+let allRecipes =[cutletFood1,cutletFood2,cutletFood3,cutletFood4,cutletFood5]
+  allRecipes.forEach((recipe)=>{
+   console.log(`${recipe.title} \n serves :${recipe.Serves} \n ${recipe.ingredients.join(" \n ")}`);
+})
