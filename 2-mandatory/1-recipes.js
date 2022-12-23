f2632636dfc78cb37cd701f060a6fd2764666f13
Hengameh M.Khani <108895187+HeniMKH@users.noreply.github.com>
2022-12-23 02:56:01 +0000

    2 ofMandatory

--- 2-mandatory/1-recipes.js
+++ 2-mandatory/1-recipes.js
@@ -23,3 +23,88 @@
 */
 
 // write code here
+let panCake1 = {
+  title : "panCake",
+  servings:5,
+  ingredients:[
+  "flour",
+  "milk",
+  "suger",
+  "egg",
+  "vanila",]
+
+
+};
+console.log(panCake1.title);
+console.log("serving:" + panCake1. servings);
+panCake1.ingredients.forEach((ingredient) => {
+  console.log(ingredient);
+})
+
+let pastaBolognese = {
+  title:"pastaBolognese",
+  servings:4,
+  ingredients:[
+  "Meat",
+  "garlic",
+  "oliveOil",
+  "onion",]
+  
+}
+console.log(pastaBolognese.title);
+console.log("serving:" + pastaBolognese.servings);
+pastaBolognese.ingredients.forEach((ingredient) => {
+  console.log(ingredient);
+})
+
+let polo = {
+  title:"polo",
+  servings:2,
+  ingredients:[
+    "rice",
+    "water",
+    "oil",
+    "salt",
+  ]
+}
+console.log(polo.title);
+console.log("serves:" + polo.servings);
+polo.ingredients.forEach((ingredient) => {
+  console.log(ingredient);
+});
+
+
+
+let cake = {
+  title:"cake",
+  servings:4,
+  ingredients:[
+    "eggs",
+    "cream",
+    "oil",
+    "icing sugar",
+    "flour"
+  ]
+}
+console.log(cake.title);
+console.log("serves:" + cake.servings);
+cake.ingredients.forEach((ingredient) => {
+  console.log(ingredient);
+});
+
+let pizza = {
+  title:"pizza",
+  serving:2,
+  ingredients:[
+    "flour",
+    "water",
+    "oil",
+    "salt",
+    "yest",
+  ]
+}
+console.log(pizza.title);
+console.log("serves:" + pizza.servings);
+pizza.ingredients.forEach((ingredient) => {
+  console.log(ingredient);
+});
