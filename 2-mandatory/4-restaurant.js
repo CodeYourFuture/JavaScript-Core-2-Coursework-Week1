952bd222ef4cbb106b17ab9054059fdbc5718722
Tony Arora <111275895+Tony-devops@users.noreply.github.com>
2023-01-05 18:26:27 +0000

    Update 4-restaurant.js

--- 2-mandatory/4-restaurant.js
+++ 2-mandatory/4-restaurant.js
@@ -22,7 +22,7 @@ const MENU = {
 let cashRegister = {
   // write code here
   orderBurger : function (balance){
-    return balance >= Menu.burger ? balance - Menu.burger : balance;
+    return balance >= MENU.burger ? balance - Menu.burger : balance;
   },
     orderFalafel: function (balance) {
      return balance >= MENU.falafel ? balance - MENU.falafel : balance;
