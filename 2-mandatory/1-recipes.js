042a9efd4b32044fc9365d9d1cc259194c2f9bb5
ys35code <ys35code@gmail.com>
2023-01-05 16:32:13 +0000

    Mandatory - 1

--- 2-mandatory/1-recipes.js
+++ 2-mandatory/1-recipes.js
@@ -23,3 +23,53 @@
 */
 
 // write code here
+
+let cake = {
+  title: "Cake",
+  servings: 2,
+  ingredients: ["cheesecake", "milk"],
+};
+
+console.log(cake.title);
+console.log("Serves:" + " " + cake.servings);
+console.log("Ingredients:" + " " + cake.ingredients.join(", "));
+
+let yogurt = {
+  title: "Yogurt",
+  servings: 3,
+  ingredients: ["milk", "water"],
+};
+
+console.log(yogurt.title);
+console.log("Serves:" + " " + yogurt.servings);
+console.log("Ingredients:" + " " + yogurt.ingredients.join(","));
+
+let cinnamon = {
+  title: "Cinnamon",
+  servings: 1,
+  ingredients: ["cinness", "cin"],
+};
+
+console.log(cinnamon.title);
+console.log("Serves:" + " " + cinnamon.servings);
+console.log("Ingredients:" + " " + cinnamon.ingredients.join(", "));
+
+let cocoa = {
+  title: "Cocoa",
+  servings: 2,
+  ingredients: ["cocoa"],
+};
+
+console.log(cocoa.title);
+console.log("Serves:" + " " + cocoa.servings);
+console.log("Ingredients:" + " " + cocoa.ingredients.join(", "));
+
+let powder = {
+  title: "Powder",
+  servings: 1,
+  ingredients: ["water"],
+};
+
+console.log(powder.title);
+console.log("Serves:" + " " + powder.servings);
+console.log("Ingredients:" + " " + powder.ingredients.join(", "));
