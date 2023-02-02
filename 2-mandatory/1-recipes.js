aca4addac0dc52363c89141199107b7bcedc54a7
Pamelalun <105378916+Pamelalun@users.noreply.github.com>
2023-02-02 12:06:37 +0000

    mandatory_Exercise1

--- 2-mandatory/1-recipes.js
+++ 2-mandatory/1-recipes.js
@@ -19,7 +19,40 @@
   cumin
   cocoa
 
+
+
   You should write and log at least 5 recipes
 */
 
 // write code here
+
+let recipes = {
+  latam:{
+    title: `papato`,
+    serves: 2,
+    ingredients: ["tomate", "garlic", "cinamon" , "water"]
+  },
+
+    comala:{
+      title: `panel`,
+      serves: 5,
+      ingredients: ["potato", "onioon", "milk" , "water"]
+  },
+
+  torta:{
+    title: `tea`,
+    serves: 3,
+    ingredients: ["claves", "cucumber", "ice" , "coffe"]
+  }
+};
+
+function printOut(food) {
+  console.log(recipes[food]["title"]);
+  console.log("serves:" + recipes[meals]["serves"]);
+  console.log("ingredients;" + recipes[meals])
+  ["ingredients"].map(x=> x + " \n ");
+}
+
+console.log("latam");
+console.log("comala");
+console.log("torta");
