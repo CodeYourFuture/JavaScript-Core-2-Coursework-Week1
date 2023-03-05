c89830c5a7d2a68146ffd5ea7a50e98e14a3782b
Dedekind561 <mitchell.lloyd154@gmail.com>
2023-03-05 23:04:08 +0000

    rename exercises dirs

---
+++ mandatory/2-currency-code-lookup.js
@@ -0,0 +1,46 @@
+/*
+  Objects can be thought of as key/value storage, like a dictionary or a 'lookup'.
+
+  You have a variable called COUNTRY_CURRENCY_CODES which is an array of arrays.
+  Each inner array contains a country code and its currency code.
+
+  Write a function called createLookup that
+    - takes COUNTRY_CURRENCY_CODES as an argument (remember, it's an array of arrays)
+    - returns a new object where the keys are the country codes and the values are the currency codes
+    Hint: you'll need to use bracket notation to add new key/value pairs to the object
+*/
+
+const COUNTRY_CURRENCY_CODES = [
+  ["GB", "GBP"],
+  ["DE", "EUR"],
+  ["NG", "NGN"],
+  ["MX", "MXN"],
+];
+
+function createLookup(countryCurrencyCodes) {
+  // write code here
+}
+
+/* ======= TESTS - DO NOT MODIFY =====
+- To run the tests for this exercise, run `npm test -- --testPathPattern 2-currency-code-lookup.js`
+- To run all exercises/tests in the mandatory folder, run `npm test`
+- (Reminder: You must have run `npm install` one time before this will work!)
+*/
+
+test("creates country code lookup for a single set of codes", () => {
+  expect(createLookup([["GB", "GBP"]])).toEqual({
+    GB: "GBP",
+  });
+  expect(createLookup([["DE", "EUR"]])).toEqual({
+    DE: "EUR",
+  });
+});
+
+test("creates country currency code lookup", () => {
+  expect(createLookup(COUNTRY_CURRENCY_CODES)).toEqual({
+    GB: "GBP",
+    DE: "EUR",
+    NG: "NGN",
+    MX: "MXN",
+  });
+});
