47a104c679cafe448b28d249513ced39f807e358
ahmedsaif2002 <ahmedsaif2002@gmail.com>
2023-03-17 05:11:54 +0000

    JavaScript-Core-2-Coursework-Week1

--- 1-exercises/A-accessing-values/exercise2.js
+++ 1-exercises/A-accessing-values/exercise2.js
@@ -17,7 +17,7 @@ let capitalCities = {
 */
 
 let myCountry = "UnitedKingdom";
-let myCapitalCity; // complete the code
+let myCapitalCity = capitalCities["UnitedKingdom"]; // using bracket notation => objectName["propertyName"]
 
 console.log(myCapitalCity);
 
