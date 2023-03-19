9dd72d1ba902a1a49b9ca102f13304abbf8a2595
Heresh <108929624+HereshT@users.noreply.github.com>
2023-03-19 08:09:31 +0000

    mandatory/1-recipes.js - The Recipe Card is Done.

--- 2-mandatory/1-recipes.js
+++ 2-mandatory/1-recipes.js
@@ -23,3 +23,47 @@
 */
 
 // write code here
+
+const recipe1 = {
+  title: "Strawberry Banana Smoothie",
+  servings: 2,
+  ingredients: ["strawberries", "banana", "orange juice"],
+};
+
+const recipe2 = {
+  title: "Mixed Berry Smoothie",
+  servings: 2,
+  ingredients: ["mixed berries", "banana", "almond milk"],
+};
+
+const recipe3 = {
+  title: "Mango Pineapple Smoothie",
+  servings: 2,
+  ingredients: ["mango", "pineapple", "coconut milk"],
+};
+
+const recipe4 = {
+  title: "Peach Raspberry Smoothie",
+  servings: 2,
+  ingredients: ["peaches", "raspberries", "vanilla yogurt"],
+};
+
+const recipe5 = {
+  title: "Kiwi Strawberry Smoothie",
+  servings: 2,
+  ingredients: ["kiwi", "strawberries", "apple juice"],
+};
+
+const smoothieRecipes = [recipe1, recipe2, recipe3, recipe4, recipe5];
+
+for (const recipe of smoothieRecipes) {
+  console.log(recipe.title);
+  console.log(`Serves: ${recipe.servings}`);
+  console.log("Ingredients:");
+  for (const ingredient of recipe.ingredients) {
+    console.log(ingredient);
+  }
+  console.log("\n");
+  console.log("🍹🍹🍹🍹🍹");
+  console.log("\n");
+}
