3d6ec7f1ca412287de0d039085c715d5df81cb49
ChristinaMifsud <christina.mifsud@gmail.com>
2023-03-29 18:49:17 +0100

    Update 3-shopping-list.js

--- 2-mandatory/3-shopping-list.js
+++ 2-mandatory/3-shopping-list.js
@@ -6,16 +6,18 @@
     - "name", which is a string
     - "ingredients", which is an array of strings
 
-  The createShoppingList function should find which ingredients from the recipe are missing from the pantry.
+  The createShoppingList function should find which ingredients from the recipe
+  are missing from the pantry.
 
   The createShoppingList function should return an object with two properties:
     - "name" of the recipe, which is a string,
     - "items", which is an array of the missing ingredients that need to be on the shopping list
 */
 
-// 2 variables - 1 string for name and 1 array for ingredients
-// compare array of ingredients to arrays in pantry object to find which are missing
-// .push the missing items into new array
+// Create 2 variables - 1 to hold name (a string) and 1 to hold ingredients (an array) - do I?
+// Compare recipe.ingredients to pantry.fridgeContents && pantry.cupboardContents
+// Find which are missing
+// .push the missing items into new array - missingItems = []
 // return name of recipe and array of missing ingredients for the shopping list
 
 let pantry = {
@@ -25,12 +27,20 @@ let pantry = {
 
 // new array with missing items (missingItems)
 function createShoppingList(recipe) {
-  let recipeName;
+  let recipeName = recipe.name;
   let recipeIngredients;
   let missingItems = [];
   let recipeObject = recipeName + missingItems;
 
+  // do I join fridgeContents & cupboardContents and then search that single array?
+
   //.filter?? to compare ingredients to pantry
+
+  let missing = pantry.filter((ingredient) => {
+    return ingredient.pantry === ingredient;
+  });
+
+  missingItems.push(missing);
 
   return recipeObject;
 }
