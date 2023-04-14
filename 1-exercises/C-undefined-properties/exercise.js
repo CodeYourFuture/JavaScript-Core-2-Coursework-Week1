26076bebe665ea881c3238bf88d9f5d198f92714
RihannaP <114884466+RihannaP@users.noreply.github.com>
2023-04-14 12:16:57 +0100

    update exercise folder

--- 1-exercises/C-undefined-properties/exercise.js
+++ 1-exercises/C-undefined-properties/exercise.js
@@ -14,11 +14,11 @@ let car = {
   yearsOld: 8,
 };
 
-console.log(car["colour"]);
+console.log(car["colour"]);  // there is no properties with colour name in the object!
 
 // Example 2
 function sayHelloToUser(user) {
-  console.log(`Hello ${user.firstName}`);
+  console.log(`Hello ${user.name}`); // in the object we have name property not firstName ,
 }
 
 let user = {
@@ -31,8 +31,11 @@ sayHelloToUser(user);
 let myPet = {
   animal: "Cat",
   getName: function() {
-    "My pet's name is Fluffy";
+    return "My pet's name is Fluffy";
   },
 };
-
-console.log(myPet.getName());
+console.log(myPet.getName()); /// we shouldn't use pranteses when we are caling properties of an object! in this way we can see
+                              // what is inside of the key and it shows it is function and its name is getName. if we want to print
+                              //function it's better to call a function with out console.log , I mean use property of object alone like
+                              // myPet.getName();
+                              // if we add return in function then we have a result that we can print!
