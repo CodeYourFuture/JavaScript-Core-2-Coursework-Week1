4a0dc26f2aad05f3e429b21aceba5c286a2d5bd6
onurat <114289826+onurat@users.noreply.github.com>
2023-04-27 17:55:12 +0100

    Update 1-recipes.js

--- 2-mandatory/1-recipes.js
+++ 2-mandatory/1-recipes.js
@@ -1,25 +1,69 @@
-/*
-  The Recipe Card
-  Never forget another recipe!
 
-  Create an object to hold information on your favorite recipe.
+const recipe1 = {
+  title: "burger",
+  servings: 2,
+  ingredients: ["beef", "bun", "cheese", "ketchup"]
+};
 
-  It should have properties for
 
-  - title (a string),
-  - servings (a number), and
-  - ingredients (an array of strings)
+console.log(recipe1.title);
+console.log(`Serves: ${recipe1.servings}`);
+console.log("Ingredients:");
+recipe1.ingredients.forEach(ingredient => console.log(ingredient));
+console.log("\n"); 
 
-  On separate lines (one console.log statement for each), log the recipe information so it looks like:
 
-  Mole
-  Serves: 2
-  Ingredients:
-  cinnamon
-  cumin
-  cocoa
+const recipe2 = {
+  title: "creamy Chicken pasta",
+  servings: 4,
+  ingredients: ["chicken breasts", "doubble cream", "chedar cheese", "mushroom", "onion", "pasta"]
+};
 
-  You should write and log at least 5 recipes
-*/
 
-// write code here
+console.log(recipe2.title);
+console.log(`Serves: ${recipe2.servings}`);
+console.log("Ingredients:");
+recipe2.ingredients.forEach(ingredient => console.log(ingredient));
+console.log("\n");
+
+
+const recipe3 = {
+  title: "Spaghetti Bolognese",
+  servings: 6,
+  ingredients: ["spaghetti", "ground beef", "onion", "garlic", "tomato sauce", "tomato paste", "red wine", "olive oil"]
+};
+
+
+console.log(recipe3.title);
+console.log(`Serves: ${recipe3.servings}`);
+console.log("Ingredients:");
+recipe3.ingredients.forEach(ingredient => console.log(ingredient));
+console.log("\n");
+
+
+const recipe4 = {
+  title: "Kuru Fasulye",
+  servings: 4,
+  ingredients: ["beans", "carrots", "beef diced", "cellerey", "onion", "spice", "peper paste"]
+};
+
+
+console.log(recipe4.title);
+console.log(`Serves: ${recipe4.servings}`);
+console.log("Ingredients:");
+recipe4.ingredients.forEach(ingredient => console.log(ingredient));
+console.log("\n"); 
+
+
+const recipe5 = {
+  title: "brownies",
+  servings: 24,
+  ingredients: ["butter", "brown sugar", "white sugar", "eggs", "vanilla extract", "flour", "baking soda", "salt", "chocolate chips"]
+};
+
+
+console.log(recipe5.title);
+console.log(`Serves: ${recipe5.servings}`);
+console.log("Ingredients:");
+recipe5.ingredients.forEach(ingredient => console.log(ingredient));
+console.log("\n");
